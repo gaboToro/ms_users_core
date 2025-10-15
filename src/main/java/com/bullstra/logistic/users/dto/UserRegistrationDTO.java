1cb150cfaa03fffa15b7ea59a64e7094a85da943
gaboToro <104457588+gaboToro@users.noreply.github.com>
2025-10-15 00:00:19 -0500

    add English comments to UserRegistrationDTO class

--- src/main/java/com/bullstra/logistic/users/dto/UserRegistrationDTO.java
+++ src/main/java/com/bullstra/logistic/users/dto/UserRegistrationDTO.java
@@ -2,11 +2,22 @@ package com.bullstra.logistic.users.dto;
 
 import lombok.Data;
 
+// DTO used for registering a new user
 @Data
 public class UserRegistrationDTO {
+
+    // User's first name
     private String name;
+
+    // User's last name
     private String lastName;
+
+    // User's email address
     private String email;
+
+    // User's password
     private String password;
+
+    // Role name assigned to the user
     private String rolName;
 }
